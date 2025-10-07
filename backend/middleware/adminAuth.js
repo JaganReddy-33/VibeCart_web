fe51f0b003365643d046d5da468878659653ff10
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2025-10-07 10:31:26 +0530

    updated case sensitive words

--- backend/middleware/adminAuth.js
+++ backend/middleware/adminAuth.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import User from "../models/user.js"; // make sure this matches your file!
+import User from "../models/User.js"; // make sure this matches your file!
 
 const adminAuth = async (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
