c5ac9070f57827b82c296a37261d812dad99cb62
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2025-11-15 14:25:09 +0530

    server js file update

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import connectDB from "./init/db.js"
+import connectDB from "./init/db.js";
+
 import productRoutes from "./routes/productRoutes.js";
 import reviewRoutes from "./routes/reviewRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -9,21 +10,26 @@ import adminRoutes from "./routes/adminRoutes.js";
 import checkoutRoutes from "./routes/checkoutRoutes.js";
 
 dotenv.config();
-connectDB(); // Database connection
+connectDB();
 
-const app=express();
+const app = express();
 app.use(express.json());
+
+// FIXED CORS
 app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://vibe-cart-web.vercel.app/",
+      "https://vibe-cart-web.vercel.app"
     ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+// Preflight fix
+app.options("*", cors());
 
 app.use("/api/products", productRoutes);
 app.use("/api/reviews", reviewRoutes);
@@ -32,8 +38,6 @@ app.use("/api/admin", adminRoutes);
 app.use("/api/checkout", checkoutRoutes);
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, ()=>{
-    console.log(`server is runninng on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`server is runninng on port ${PORT}`);
 });
-
