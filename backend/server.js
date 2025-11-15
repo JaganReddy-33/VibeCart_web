655f5c86ebe8e9bfbaf8762f35d2d56fd3829d43
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2025-11-15 12:58:18 +0530

    changed base url

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,16 @@ connectDB(); // Database connection
 
 const app=express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://vibe-cart-frontend.vercel.app"
+    ],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use("/api/products", productRoutes);
 app.use("/api/reviews", reviewRoutes);
